b63c1d0b9abb9b1096e6a1d0f2a28e3ab53bdda0
kms <beckum8282@naver.com>
2021-05-08 22:13:07 +0900

    Solved Yaksu

--- Codes/YakSu.cpp
+++ Codes/YakSu.cpp
@@ -3,16 +3,26 @@ using namespace std;
 int main(){
     int i = 0,n=0, max = 0, min = 0;
     cin >> i; // i는 약수 갯수
+    int arr[i];
     for(int j=0;j<i;j++)
     {
-        cin >> n;
-        if(max < n)
+        cin >> arr[j];
+        if(max < arr[j])
         {
-            max = n;
+            max = arr[j];
         }
     }
-    
-
-
+    for(int j=0;j<i;j++)
+    {
+        if(j==0)
+        {
+            min = arr[j];
+        }
+        if(min > arr[j])
+        {
+            min = arr[j];
+        }
+    }
+    cout << max*min;
 }
 // https://www.acmicpc.net/problem/1037 약수 문제
